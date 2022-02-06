26ffb3603698373673aa57ba927f45810c675e84
Kukonen <ekukonen@gmail.com>
2022-02-06 19:48:08 +0300

    add HOC as components

---
+++ client/components/docs.component.template.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/DocsComponent.module.scss';
+import {DetailedHTMLProps, HTMLAttributes, ReactNode} from "react";
+
+interface ComponentTemplateProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+    title: string;
+    children: ReactNode;
+}
+
+const ComponentTemplate = ({title, children, ...props} : ComponentTemplateProps) : JSX.Element => {
+    return (
+        <div className={styles.container} {...props}>
+            <div className={styles.headline}>
+                {title}
+            </div>
+            <div className={styles.content}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default ComponentTemplate;
